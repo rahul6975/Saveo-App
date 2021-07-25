fe48fa8c2ef53a5ed151c5125aa929ac18f55379
rahul6975 <rahulya7569@gmail.com>
2021-07-25 11:55:29 +0530

    packages created

--- app/src/main/java/com/rahul/saveoapp/views/MainActivity.kt
+++ app/src/main/java/com/rahul/saveoapp/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rahul.saveoapp
+package com.rahul.saveoapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rahul.saveoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
